471cba0aa25e8a04f87fce86e3180011dbfb572e
Bernd Haberstumpf <poldi@thatswing.de>
2024-05-01 23:53:00 +0200

    Add backup files to replace_quote_char.sh

--- lib/replace_quote_char.sh
+++ lib/replace_quote_char.sh
@@ -13,9 +13,11 @@ fi
 
 for i in $*; do
   if [ -z "$REVERT" ]; then
+    cp "$i" "$i.7bit.bak"
     echo "Converting $i \"a  etc.  to ä..."
     ex +'%s/"a/ä/g' +'%s/"o/ö/g' +'%s/"u/ü/g' +'%s/"A/Ä/g' +'%s/"O/Ö/g' +'%s/"U/Ü/g' +'%s/\\3/ß/g' +'wq' $i
   else
+    cp "$i" "$i.8bit.bak"
     echo "Converting $i ä  etc.  to \"a..."
     ex +'%s/ä/"a/g' +'%s/ö/"o/g' +'%s/ü/"u/g' +'%s/Ä/"A/g' +'%s/Ö/"O/g' +'%s/Ü/"U/g' +'%s/ß/\\3/g' +'wq' $i
   fi
