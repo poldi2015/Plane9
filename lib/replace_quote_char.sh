930666b53b09cf5e0cac9defbcdb3bbffd831b9d
Bernd Haberstumpf <poldi@thatswing.de>
2024-08-06 18:23:52 +0800

    Improved replace_quote_char.sh

--- lib/replace_quote_char.sh
+++ lib/replace_quote_char.sh
@@ -24,10 +24,10 @@ for i in $FILES; do
   if [ -z "$REVERT" ]; then
     cp "$i" "$i.7bit.bak"
     echo "Converting $i \"a  etc.  to ä..."
-    ex +'%s/"a/ä/g' +'%s/"o/ö/g' +'%s/"u/ü/g' +'%s/"A/Ä/g' +'%s/"O/Ö/g' +'%s/"U/Ü/g' +'%s/\\3/ß/g' +'wq' $i
+    ex +'%s/"a/ä/g' +'%s/"o/ö/g' +'%s/"u/ü/g' +'%s/"A/Ä/g' +'%s/"O/Ö/g' +'%s/"U/Ü/g' +'%s/\\3/ß/g' +"%s/\\\\'e/é/g" +'wq' $i
   else
     cp "$i" "$i.8bit.bak"
     echo "Converting $i ä  etc.  to \"a..."
-    ex +'%s/ä/"a/g' +'%s/ö/"o/g' +'%s/ü/"u/g' +'%s/Ä/"A/g' +'%s/Ö/"O/g' +'%s/Ü/"U/g' +'%s/ß/\\3/g' +'wq' $i
+    ex +'%s/ä/"a/g' +'%s/ö/"o/g' +'%s/ü/"u/g' +'%s/Ä/"A/g' +'%s/Ö/"O/g' +'%s/Ü/"U/g' +'%s/ß/\\3/g' +"%s/é/\\\\'e/g" +'wq' $i
   fi
 done
